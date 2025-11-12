21197a51822b135ce9c3e523042c3af6088285df
casav929 <casav929@student.liu.se>
2025-11-12 16:04:20 +0100

    Added demo for reference

---
+++ Demo/Demo/ViewModel/Command/StartGameCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Demo.ViewModel;
+
+namespace Demo.View.Command
+{
+    internal class StartGameCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private MainWindowViewModel parent = null;
+
+
+        public StartGameCommand(MainWindowViewModel parent)
+        {
+            this.parent = parent;
+
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+
+            parent.startGameBoard();
+        }
+    }
+}
